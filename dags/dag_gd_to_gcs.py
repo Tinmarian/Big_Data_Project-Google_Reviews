d81401b21aa28852ad455d875060de61b9d51753
Tinmarian <tinmar96@gmail.com>
2023-10-01 20:25:25 -0600

    debugging dag_gd_to_gcs: final commits

--- dags/dag_gd_to_gcs.py
+++ dags/dag_gd_to_gcs.py
@@ -54,33 +54,33 @@ def gd_to_gcs():
 		gcp_conn_id = GCPCONN
 	)
 
-	MY_FOLDER_ID = '1y1XFty1aIIvAix3F8qpJMqnC_YiTIXyP'
-# Probando funcionalidad
-	prueba1 = GoogleDriveToGCSOperator(
-		task_id = 'prueba1',
-		bucket_name=MY_BUCKET_NAME,
-		object_name='prueba1.docx',
-		file_name='Diccionario de datos.docx',
-		folder_id=MY_FOLDER_ID,
-		# drive_id=SDRIVE_ID,
-		gcp_conn_id=GCPCONN
-	)
+# 	MY_FOLDER_ID = '1y1XFty1aIIvAix3F8qpJMqnC_YiTIXyP'
+# # Probando funcionalidad
+# 	prueba1 = GoogleDriveToGCSOperator(
+# 		task_id = 'prueba1',
+# 		bucket_name=MY_BUCKET_NAME,
+# 		object_name='review1.docx',
+# 		file_name='review_tx_1.json',
+# 		folder_id= 'my-drive',# MY_FOLDER_ID,
+# 		# drive_id=SDRIVE_ID,
+# 		gcp_conn_id=GCPCONN
+# 	)
 
-	# SDRIVE_ID = '0AOid89EWziepUk9PVA'
-	prueba2 = GoogleDriveToGCSOperator( # Se encontró un inconveniente al transferir archivos que sean documentos de google, para ello habrá que utilizar la API y el método "export_media" y convertirlos a formatos aceptables.
-		task_id = 'prueba2',
-		bucket_name=MY_BUCKET_NAME,
-		object_name='prueba2',
-		file_name='Copia de Temas emergentes.docx',
-		folder_id=MY_FOLDER_ID,
-		# drive_id=SDRIVE_ID,
-		gcp_conn_id=GCPCONN
-	)
+# 	# SDRIVE_ID = '0AOid89EWziepUk9PVA'
+# 	prueba2 = GoogleDriveToGCSOperator( # Se encontró un inconveniente al transferir archivos que sean documentos de google, para ello habrá que utilizar la API y el método "export_media" y convertirlos a formatos aceptables.
+# 		task_id = 'prueba2',
+# 		bucket_name=MY_BUCKET_NAME,
+# 		object_name='prueba2',
+# 		file_name='Copia de Temas emergentes.docx',
+# 		folder_id= MY_FOLDER_ID,
+# 		# drive_id=SDRIVE_ID,
+# 		gcp_conn_id=GCPCONN
+# 	)
 
 # Carga de datos formal
 	# Carga de datos Yelp
-	MY_FOLDER_ID = '1TI-SsMnZsNP6t930olEEWbBQdo_yuIZF' # Folder de Yelp
-	for MY_FILE_NAME in ['user.parquet','tip.json','review.json','business.pkl']:
+	MY_FOLDER_ID = '1IcC0SiBY2UeyRUn7gcs2PfIhwptvDRdk' # Folder de Yelp
+	for MY_FILE_NAME in ['user.parquet','tip.json','review.json','business.pkl','checkin.json']:
 		extract_load_yelp = GoogleDriveToGCSOperator(
 			task_id = f'extract_load_yelp_{MY_FILE_NAME}',
 			bucket_name=MY_BUCKET_NAME,
@@ -93,7 +93,7 @@ def gd_to_gcs():
 		)
 
 	# Cargar datos de Maps Metadata
-	MY_FOLDER_ID = '1olnuKLjT8W2QnCUUwh8uDuTTKVZyxQ0Z'
+	MY_FOLDER_ID = '1FrOLWoLdPDyi1h7BKT5z-LSGjuMxIyKb'
 	for MY_FILE_NAME in range(1,12):
 		extract_load_maps_meta = GoogleDriveToGCSOperator(
 			task_id = f'extract_load_maps_meta_{MY_FILE_NAME}',
@@ -107,72 +107,71 @@ def gd_to_gcs():
 		)
 
 	# Cargar datos de Maps Estados
-	for OBJECT_NAME,MY_FOLDER_ID in zip(['review-New_York','review-California','review-Texas','review-Colorado','review-Georgia'],['18HYLDXcKg-cC1CT9vkRUCgea04cNpV33','1Jrbjt-0hnLCvecfrnMwGu1jYZSxElJll','1zq12pojMW2zeGgts0lHFSf_pF1L_4UWr','1IlUZJZxOyRiIWo3G6BqW1Thu2kXYKMFX','1MuPznes6CebS6gyWPVU-kR4EVKKLY4l3']):
-		if OBJECT_NAME == 'review-New_York':
-			for MY_FILE_NAME in range(1,19):
-				extract_load_maps_newyork = GoogleDriveToGCSOperator(
-					task_id = f'extract_load_maps_newyork_{MY_FILE_NAME}',
-					bucket_name=MY_BUCKET_NAME,
-					object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
-					file_name=f'{MY_FILE_NAME}.json',
-					folder_id=MY_FOLDER_ID,
-					# drive_id=MY_FOLDER_ID,
-					gcp_conn_id=GCPCONN,
-					trigger_rule='all_success'
-				)
+	OBJECT_NAME,MY_FOLDER_ID = ['review-New_York','review-California','review-Texas','review-Colorado','review-Georgia'],['1fNf7hKTn9W3XVtbeKN_w7G_eVVviFCR0','1ikEacrw5YaD3_yZQK7OOArN1VjGXJOhh','1u-q7_KBPQlKuICsjorm0nQR8CRhkImKc','13FmmDFGjcMacTm9bLEeq9yO7BESLprIK','1bB7j6_ojiSZSm7WYZ2Sue67qrShQNMit']
 
-		if OBJECT_NAME == 'review-California':
-			for MY_FILE_NAME in range(1,19):
-				extract_load_maps_california = GoogleDriveToGCSOperator(
-					task_id = f'extract_load_maps_california_{MY_FILE_NAME}',
-					bucket_name=MY_BUCKET_NAME,
-					object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
-					file_name=f'{MY_FILE_NAME}.json',
-					folder_id=MY_FOLDER_ID,
-					# drive_id=MY_FOLDER_ID,
-					gcp_conn_id=GCPCONN,
-					trigger_rule='all_success'
-				)
+	for MY_FILE_NAME in range(1,19):
+		extract_load_maps_newyork = GoogleDriveToGCSOperator(
+			task_id = f'extract_load_maps_newyork_{MY_FILE_NAME}',
+			bucket_name=MY_BUCKET_NAME,
+			object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
+			file_name=f'{MY_FILE_NAME}.json',
+			folder_id=MY_FOLDER_ID[0],
+			# drive_id=MY_FOLDER_ID,
+			gcp_conn_id=GCPCONN,
+			trigger_rule='all_success'
+		)
 
-		if OBJECT_NAME == 'review-Texas':
-			for MY_FILE_NAME in range(1,17):
-				extract_load_maps_texas = GoogleDriveToGCSOperator(
-					task_id = f'extract_load_maps_texas_{MY_FILE_NAME}',
-					bucket_name=MY_BUCKET_NAME,
-					object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
-					file_name=f'{MY_FILE_NAME}.json',
-					folder_id=MY_FOLDER_ID,
-					# drive_id=MY_FOLDER_ID,
-					gcp_conn_id=GCPCONN,
-					trigger_rule='all_success'
-					)
+	for MY_FILE_NAME in range(1,19):
+		extract_load_maps_california = GoogleDriveToGCSOperator(
+			task_id = f'extract_load_maps_california_{MY_FILE_NAME}',
+			bucket_name=MY_BUCKET_NAME,
+			object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
+			file_name=f'{MY_FILE_NAME}.json',
+			folder_id=MY_FOLDER_ID[1],
+			# drive_id=MY_FOLDER_ID,
+			gcp_conn_id=GCPCONN,
+			trigger_rule='all_success'
+		)
 
-		if OBJECT_NAME == 'review-Colorado':
-			for MY_FILE_NAME in range(1,17):
-				extract_load_maps_colorado = GoogleDriveToGCSOperator(
-					task_id = f'extract_load_maps_colorado_{MY_FILE_NAME}',
-					bucket_name=MY_BUCKET_NAME,
-					object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
-					file_name=f'{MY_FILE_NAME}.json',
-					folder_id=MY_FOLDER_ID,
-					# drive_id=MY_FOLDER_ID,
-					gcp_conn_id=GCPCONN,
-					trigger_rule='all_success'
-				)
+	for MY_FILE_NAME in range(1,17):
+		extract_load_maps_texas = GoogleDriveToGCSOperator(
+			task_id = f'extract_load_maps_texas_{MY_FILE_NAME}',
+			bucket_name=MY_BUCKET_NAME,
+			object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
+			file_name=f'{MY_FILE_NAME}.json',
+			folder_id=MY_FOLDER_ID[2],
+			# drive_id=MY_FOLDER_ID,
+			gcp_conn_id=GCPCONN,
+			trigger_rule='all_success'
+			)
 
-		if OBJECT_NAME == 'review-Georgia':
-			for MY_FILE_NAME in range(1,14):
-				extract_load_maps_georgia = GoogleDriveToGCSOperator(
-					task_id = f'extract_load_maps_georgia_{MY_FILE_NAME}',
-					bucket_name=MY_BUCKET_NAME,
-					object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
-					file_name=f'{MY_FILE_NAME}.json',
-					folder_id=MY_FOLDER_ID,
-					# drive_id=MY_FOLDER_ID,
-					gcp_conn_id=GCPCONN,
-					trigger_rule='all_success'
-				)
+	for MY_FILE_NAME in range(1,17):
+		extract_load_maps_colorado = GoogleDriveToGCSOperator(
+			task_id = f'extract_load_maps_colorado_{MY_FILE_NAME}',
+			bucket_name=MY_BUCKET_NAME,
+			object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
+			file_name=f'{MY_FILE_NAME}.json',
+			folder_id=MY_FOLDER_ID[3],
+			# drive_id=MY_FOLDER_ID,
+			gcp_conn_id=GCPCONN,
+			trigger_rule='all_success'
+		)
 
-	delete_bucket >> create_bucket >> [prueba1,prueba2] >> extract_load_maps_meta >> extract_load_yelp >> extract_load_maps_newyork >> extract_load_maps_california >> extract_load_maps_texas >> extract_load_maps_colorado >> extract_load_maps_georgia
+	
+	for MY_FILE_NAME in range(1,14):
+		extract_load_maps_georgia = GoogleDriveToGCSOperator(
+			task_id = f'extract_load_maps_georgia_{MY_FILE_NAME}',
+			bucket_name=MY_BUCKET_NAME,
+			object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
+			file_name=f'{MY_FILE_NAME}.json',
+			folder_id=MY_FOLDER_ID[4],
+			# drive_id=MY_FOLDER_ID,
+			gcp_conn_id=GCPCONN,
+			trigger_rule='all_success'
+		)
+
+	# delete_bucket >> create_bucket >> [prueba1,prueba2] >> extract_load_maps_meta >> extract_load_yelp >> extract_load_maps_newyork >> extract_load_maps_california >> extract_load_maps_texas >> extract_load_maps_colorado >> extract_load_maps_georgia
+
+	delete_bucket >> create_bucket >> extract_load_maps_meta >> extract_load_yelp >> extract_load_maps_newyork >> extract_load_maps_california >> extract_load_maps_texas >> extract_load_maps_colorado >> extract_load_maps_georgia
 
 dag = gd_to_gcs()
