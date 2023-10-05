9ac9fdc2a18e38e3a981ea87a4fa0b96261e7f97
Tinmarian <tinmar96@gmail.com>
2023-10-04 20:40:02 -0600

    debuggin dataproc pyspark job on dag_gcs_to_dataproc

--- pyspark_jobs/cleaning_job.py
+++ pyspark_jobs/cleaning_job.py
@@ -12,7 +12,7 @@ import pyspark.pandas as ps
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../credentials/fiery-protocol-399500-f2566dd92ef4.json'
 PROJECT_ID = 'fiery-protocol-399500'
-states = ['California','Texas','New_York','Colorado','Georgia']
+STATES = ['California','Texas','New_York','Colorado','Georgia']
 schema = StructType([
     StructField('user_id',LongType(),False),
     StructField('name',StringType(),True),
@@ -22,25 +22,10 @@ schema = StructType([
     StructField('resp',MapType(StringType(),StringType(),True),True),
     StructField('gmap_id',StringType(),False)
 ])
-
-
-
-
 # Create a SparkSession
 spk = SparkSession.builder.appName("PySpark Transformations to Populate our Data Warehouse").getOrCreate()
 
 
-
-# fs = gcsfs.GCSFileSystem(project=PROJECT_ID)
-# i = 1
-# dfx = pd.DataFrame()
-# for state in states:
-#     while True:
-#         try:
-#             with fs.open(f'/{state}_{i}.json') as f:
-#                 df = pd.read_json(f'gs://data-lake-henry/{state}_{i}.json')
-#         except AnalysisException:
-#             break
 
 
 # # Cargamos los metadatos de Google Maps.
@@ -73,42 +58,36 @@ spk = SparkSession.builder.appName("PySpark Transformations to Populate our Data
 # logging.info(f'Pequeñas transformaciones')
 
 
+
+
+
+
+
+
+
 # Concatenamos los diferentes archivos de reviews que tenemos para cada estado.
 psdfx = ps.DataFrame()
-for state in states:
+for state in STATES:
     i = 1
     df_list = []
     while True:
         try:
             # Leemos los archivos en un SPARK Data Frame para poder acceder directamente a GCS
-            sdf1 = spk.read.text(f'gs://data-lake-henry/{state}_{i}.json')
-            sdf1.write.mode('overwrite').format('json').save(f'gs://dataproc-pyspark-ops/out_dataproc/{state}_{i}_text')
-            sdf2 = spk.read.json(f'gs://data-lake-henry/{state}_{i}.json',multiLine=True,mode='DROPMALFORMED')
-            sdf2.write.mode('overwrite').format('json').save(f'gs://dataproc-pyspark-ops/out_dataproc/{state}_{i}_json')
-            sdf3 = spk.read.format('json').load(f'gs://data-lake-henry/{state}_{i}.json')
-            sdf3.write.mode('overwrite').format('json').save(f'gs://dataproc-pyspark-ops/out_dataproc/{state}_{i}_load'))
             sdf = spk.read.schema(schema).json(f'gs://data-lake-henry/{state}_{i}.json',multiLine=True,mode='DROPMALFORMED')[['user_id','name','time','rating','text','resp','gmap_id']]
-            logging.info(f'Cargo archivo {state}_{i}')
-            # PANDAS API Data Frame: Paso intermedio para generar un PANDAS Data Frame.
+            # PANDAS API Data Frame: Paso intermedio para trabajar con los métodos de pandas pero con la potencia de spark, posteriormente guardaremos los datos en BQ después de 
+            # las transformaciones...
             psdf = sdf.pandas_api()
             psdf['estado'] = state
             df_list.append(psdf)
             i += 1
         except AnalysisException:
             break
-        
-
-    logging.info(f'Pequeñas transformaciones')
 
     psdfx = ps.concat(df_list,axis=0)
 
-    logging.info(f'Archivos de reviews concatenados')
-
-    psdfx.to_json('gs://dataproc-pyspark-ops/out_dataproc/conc_prev_table.json')
-
     # Generamos el primer grupo de transformaciones para los datos de las reviews de Maps en PANDAS API. Queda la metadata y los archivos de Yelp.
-    psdfx['resp_time'] = ps.Series(dtype=np.int64)
-    psdfx['resp_text'] = ps.Series(dtype=np.int64)
+    psdfx['resp_time'] = ps.Series(dtype=LongType)
+    psdfx['resp_text'] = ps.Series(dtype=LongType)
     for i in range(len(psdfx)):
         if type(psdfx.resp[i]) == dict:
             psdfx.loc[i,'resp_time'] = psdfx.resp[i]['time']
@@ -116,9 +95,9 @@ for state in states:
         else:
             psdfx.loc[i,'resp_time'] = 1
             psdfx.loc[i,'resp_text'] = ''
-    psdfx.resp_time = psdfx.resp_time.fillna(0).astype('int64')
+    psdfx.resp_time = psdfx.resp_time.fillna(0).astype('int')
     psdfx.resp_text = psdfx.resp_text.fillna('')
-    psdfx = psdfx[['user_id', 'name', 'time', 'rating', 'text', 'gmap_id', 'resp_time', 'resp_text']]
+    psdfx = psdfx[['gmap_id','user_id','name','time','text','rating','resp_time','resp_text']]
 
     # Convertimos el dataframe de Pandas API on Spark a un dataframe de Spark
     sdf = psdf.to_spark()
