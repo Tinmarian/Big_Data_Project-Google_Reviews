303e2c55668ccb70bf0d7608c04727857abd265c
Tinmarian <tinmar96@gmail.com>
2023-10-06 23:12:20 -0600

    debugging dataproc pyspark job on dag_gcs_to_dataproc

--- dags/dag_gcs_to_dataproc.py
+++ dags/dag_gcs_to_dataproc.py
@@ -1,11 +1,14 @@
-from airflow.decorators import dag,task_group
+from airflow.decorators import dag
 
 from airflow.providers.google.cloud.operators.dataproc import DataprocCreateClusterOperator, DataprocSubmitJobOperator, DataprocDeleteClusterOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryCreateEmptyDatasetOperator
 from airflow.providers.google.cloud.operators.gcs import GCSDeleteObjectsOperator
 
-from datetime import datetime, timedelta
+from datetime import datetime
+
+
+
 
 GCPCONN = "google_cloud_henry"
 PROJECT_ID = 'fiery-protocol-399500'
@@ -13,30 +16,27 @@ CLUSTER_NAME = 'test-dataproc-henry'
 REGION = 'us-east1'
 MY_BUCKET_NAME = 'dataproc-pyspark-ops'
 DATASET = 'staging_fph'
-STATES = sorted(['California','Texas','New_York','Colorado','Georgia'])
-
 PYSPARK_URI = 'gs://dataproc-pyspark-ops/pyspark-jobs/cleaning-stage/cleaning_job.py'
 CLUSTER_CONFIG = {
     "master_config" : {
-        "machine_type_uri" : "n2-standard-2",
+        "machine_type_uri" : "e2-standard-4",
         "disk_config" : {"boot_disk_type" : "pd-standard", "boot_disk_size_gb" : 75}
     },
     "worker_config" : {
         "num_instances" : 2,
-        "machine_type_uri" : "e2-standard-8",
+        "machine_type_uri" : "c3d-highcpu-4",
+        "threads" : 1,
         "disk_config" : {"boot_disk_type" : "pd-standard", "boot_disk_size_gb" : 75}
     },
     "software_config" : {
         "image_version" : '2.1.25-ubuntu20'
     }
 }
-
 PYSPARK_JOB = {
     "reference" : {"project_id":PROJECT_ID},
     "placement" : {"cluster_name":CLUSTER_NAME},
     "pyspark_job": {"main_python_file_uri":PYSPARK_URI,}
 }
-
 default_args = {
     "owner" : 'Tinmar Andrade',
     'start_date':datetime(2023,9,30),
@@ -44,6 +44,9 @@ default_args = {
     'email':['tinmar96@gmail.com','jozz.rom@gmail.com'],
     'email_on_failure':True
 }
+
+
+
 
 @dag(
     'gcs_to_dataproc_cleaning_to_bq',
@@ -58,7 +61,7 @@ def gcs_to_dataproc():
                                                         gcp_conn_id=GCPCONN,
                                                         dataset_id=DATASET,
                                                         project_id=PROJECT_ID,
-                                                        location='us-east1'
+                                                        location=REGION
                                                     )
 
     create_cluster = DataprocCreateClusterOperator(
@@ -80,36 +83,32 @@ def gcs_to_dataproc():
                                     trigger_rule='all_success'
                                 )
 
-    @task_group(group_id='unique_state_tables')
-    def tg1(states):
-        i = 0
-        for state in states:
-            GCS_to_BQ = GCSToBigQueryOperator(
-                                                task_id = f'gcs_to_bq_after_cleaning_{state}',
-                                                bucket=MY_BUCKET_NAME,
-                                                source_objects=[f'out_dataproc/staging/all_{state}_raw/*.csv'],
-                                                destination_project_dataset_table= f'{DATASET}.dcs_review_{state}',
-                                                schema_fields=[{'name':'user_id','type':'INT64','mode':'REQUIRED'}, 
-                                                                {'name':'name','type':'STRING','mode':'NULLABLE'}, 
-                                                                {'name':'time','type':'INT64','mode':'REQUIRED'}, 
-                                                                {'name':'rating','type':'INT64','mode':'NULLABLE'}, 
-                                                                {'name':'text','type':'STRING','mode':'NULLABLE'}, 
-                                                                {'name':'gmap_id','type':'STRING','mode':'REQUIRED'}, 
-                                                                {'name':'resp_time','type':'INT64','mode':'NULLABLE'}, 
-                                                                {'name':'resp_text','type':'STRING','mode':'NULLABLE'}
-                                                            ],
-                                                autodetect=True,
-                                                source_format="CSV",
-                                                create_disposition="CREATE_IF_NEEDED",
-                                                skip_leading_rows="None",
-                                                write_disposition="WRITE_TRUNCATE", # WRITE_APPEND,
-                                                field_delimiter=",",
-                                                encoding="UTF-8",
-                                                gcp_conn_id=GCPCONN,
-                                                location="us-east1",
-                                                job_id=f"ETL-gcs_to_dataproc_to_bq-{state}"
-                                            )
-            i += 1
+
+    GCS_to_BQ = GCSToBigQueryOperator(
+                                        task_id = f'gcs_to_bq_after_cleaning_reviews',
+                                        bucket=MY_BUCKET_NAME,
+                                        source_objects=[f'out_dataproc/staging/table_of_states/*.csv'],
+                                        destination_project_dataset_table= f'{DATASET}.dcs_reviews_all_states',
+                                        schema_fields=[{'name':'user_id','type':'INT64','mode':'REQUIRED'}, 
+                                                        {'name':'name','type':'STRING','mode':'NULLABLE'}, 
+                                                        {'name':'time','type':'INT64','mode':'REQUIRED'}, 
+                                                        {'name':'rating','type':'INT32','mode':'NULLABLE'}, 
+                                                        {'name':'text','type':'STRING','mode':'NULLABLE'}, 
+                                                        {'name':'gmap_id','type':'STRING','mode':'REQUIRED'}, 
+                                                        {'name':'resp_time','type':'INT64','mode':'NULLABLE'}, 
+                                                        {'name':'resp_text','type':'STRING','mode':'NULLABLE'}
+                                                    ],
+                                        autodetect=True,
+                                        source_format="CSV",
+                                        create_disposition="CREATE_IF_NEEDED",
+                                        skip_leading_rows="None",
+                                        write_disposition="WRITE_TRUNCATE", # WRITE_APPEND,
+                                        field_delimiter=",",
+                                        encoding="UTF-8",
+                                        gcp_conn_id=GCPCONN,
+                                        location=REGION,
+                                        job_id=f"ETL-gcs_to_dataproc_to_bq_states"
+                                    )
 
     delete_cluster = DataprocDeleteClusterOperator(
                                                     task_id='delete_dataproc_cluster',
@@ -120,15 +119,16 @@ def gcs_to_dataproc():
                                                     trigger_rule='all_done'
                                                 )
 
-    delete_folder = GCSDeleteObjectsOperator(
+    delete_staging_tables = GCSDeleteObjectsOperator(
                                                 task_id='delete_output_folder_pyspark_job',
                                                 gcp_conn_id=GCPCONN,
                                                 bucket_name=MY_BUCKET_NAME,
                                                 prefix='out_dataproc/staging/',
-                                                objects=['*/']
+                                                objects=['*'],
+                                                trigger_rule='all_done'
                                             )
 
-    create_dataset >> create_cluster >> job >> tg1(STATES) >> [delete_folder,delete_cluster] 
+    create_dataset >> create_cluster >> job >> GCS_to_BQ >> [delete_staging_tables,delete_cluster] 
 
 
 dag = gcs_to_dataproc()
--- pyspark_jobs/cleaning_job.py
+++ pyspark_jobs/cleaning_job.py
@@ -1,7 +1,6 @@
-import os
 import pandas as pd
-import numpy as np
-import logging
+# import numpy as np
+import os
 # import gcsfs
 
 
@@ -10,22 +9,21 @@ from pyspark.sql.utils import AnalysisException
 from pyspark.sql.types import IntegerType,StructField,StructType,StringType,LongType,MapType
 import pyspark.pandas as ps
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../credentials/fiery-protocol-399500-f2566dd92ef4.json'
+os.environ["PYARROW_IGNORE_TIMEZONE"] = '1' 
 PROJECT_ID = 'fiery-protocol-399500'
-STATES = ['California','Texas','New_York','Colorado','Georgia']
+STATES = ['California','Texas'] # ,'New_York','Colorado','Georgia']
 schema = StructType([
-    StructField('user_id',LongType(),False),
+    StructField('user_id',StringType(),False),
     StructField('name',StringType(),True),
     StructField('time',LongType(),True),
     StructField('rating',IntegerType(),True),
     StructField('text',StringType(),True),
-    StructField('resp',MapType(StringType(),StringType(),True),True),
+    StructField('resp',MapType(StringType(),StringType()),True),
     StructField('gmap_id',StringType(),False)
 ])
 # Create a SparkSession
 spk = SparkSession.builder.appName("PySpark Transformations to Populate our Data Warehouse").getOrCreate()
-
-
+ps.options.compute.ops_on_diff_frames = True
 
 
 # # Cargamos los metadatos de Google Maps.
@@ -66,41 +64,50 @@ spk = SparkSession.builder.appName("PySpark Transformations to Populate our Data
 
 
 # Concatenamos los diferentes archivos de reviews que tenemos para cada estado.
-psdfx = ps.DataFrame()
+psdfx = ps.DataFrame(columns=['gmap_id','user_id','name','time','text','rating','resp_time','resp_text'])
 for state in STATES:
     i = 1
     df_list = []
     while True:
         try:
             # Leemos los archivos en un SPARK Data Frame para poder acceder directamente a GCS
-            sdf = spk.read.schema(schema).json(f'gs://data-lake-henry/{state}_{i}.json',multiLine=True,mode='DROPMALFORMED')[['user_id','name','time','rating','text','resp','gmap_id']]
+            sdf = spk.read.schema(schema).json(f'gs://data-lake-henry/{state}_{i}.json')[['user_id','name','time','rating','text','resp','gmap_id']]
+            sdf.selectExpr('cast(user_id as int) user_id')
             # PANDAS API Data Frame: Paso intermedio para trabajar con los métodos de pandas pero con la potencia de spark, posteriormente guardaremos los datos en BQ después de 
             # las transformaciones...
+            # sdf.count()
             psdf = sdf.pandas_api()
+            psdf['time'] = ps.to_datetime(psdf['time'],unit='ms')            
             psdf['estado'] = state
             df_list.append(psdf)
             i += 1
         except AnalysisException:
             break
 
-    psdfx = ps.concat(df_list,axis=0)
+    psdf = ps.concat(df_list,axis=0)
+    psdf.spark.persist()
 
     # Generamos el primer grupo de transformaciones para los datos de las reviews de Maps en PANDAS API. Queda la metadata y los archivos de Yelp.
-    psdfx['resp_time'] = ps.Series(dtype=int)
-    psdfx['resp_text'] = ps.Series(dtype=int)
-    for i in range(len(psdfx)):
-        if type(psdfx.resp[i]) == dict:
-            psdfx.loc[i,'resp_time'] = psdfx.resp[i]['time']
-            psdfx.loc[i,'resp_text'] = psdfx.resp[i]['text']
+    psdf['resp_time'] = ps.Series([],dtype='int64')
+    psdf['resp_text'] = ps.Series([],dtype='str')
+    # psdfx.astype({'resp_time' : 'int64'})
+    # psdfx.astype({'resp_time' : str})
+    for i in range(len(psdf)):
+        if type(psdf.resp[i]) == type(dict):
+            psdf.loc[i,'resp_time'] = psdf.resp[i]['time']
+            psdf.loc[i,'resp_text'] = psdf.resp[i]['text']
         else:
-            psdfx.loc[i,'resp_time'] = 1
-            psdfx.loc[i,'resp_text'] = ''
-    psdfx.resp_time = psdfx.resp_time.fillna(0).astype(LongType)
-    psdfx.resp_text = psdfx.resp_text.fillna('')
-    psdfx = psdfx[['gmap_id','user_id','name','time','text','rating','resp_time','resp_text']]
+            psdf.loc[i,'resp_time'] = 0
+            psdf.loc[i,'resp_text'] = ''
+    psdf.resp_time = psdf.resp_time.fillna(0).astype(LongType)
+    psdf.resp_text = psdf.resp_text.fillna('')
+    psdf = psdf[['gmap_id','user_id','name','time','text','rating','resp_time','resp_text']]
 
-    # Convertimos el dataframe de Pandas API on Spark a un dataframe de Spark
-    sdf = psdf.to_spark()
+    # Aquí concatenamos todos los archivos del estado en curso a los demás estados, para obtener una tabla total de estados.
+    psdfx = ps.concat(psdf,axis=0)
 
-    # Guardamos las tablas concatenadas en archivos .json en GCS.
-    sdf.write.mode('overwrite').format('csv').save(f'gs://dataproc-pyspark-ops/out_dataproc/staging/all_{state}_raw')
+# Convertimos el dataframe de Pandas API on Spark a un dataframe de Spark
+sdf = psdf.to_spark()
+
+# Guardamos las tablas concatenadas en archivos .json en GCS.
+sdf.write.mode('overwrite').format('csv').save(f'gs://dataproc-pyspark-ops/out_dataproc/staging/table_of_states')
